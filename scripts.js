7c2a1c4387fdac5cf13e63c142448f074fcfda6b
Mord Maman <jaime.mord.maman@gmail.com>
2017-01-12 10:29:37 +0000

    starts working on section 4

--- scripts.js
+++ scripts.js
@@ -1,10 +1,5 @@
 $(document).ready(function(){
   $('.bxslider').bxSlider();
 
-  $('#demo').scrollbox({
-        linear: true,
-        step: 1,
-        delay: 0,
-        speed: 100
-    });
 });
+
