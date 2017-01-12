edab0b1f28ba148938b376b8772e640ea37a4500
Mord Maman <jaime.mord.maman@gmail.com>
2017-01-12 09:42:50 +0000

    adds jquery  scroll box and fixes height

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
   $('.bxslider').bxSlider();
+
+  $('#demo').scrollbox({
+        linear: true,
+        step: 1,
+        delay: 0,
+        speed: 100
+    });
 });
